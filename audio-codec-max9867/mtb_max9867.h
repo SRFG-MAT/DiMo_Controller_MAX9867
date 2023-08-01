4f85047365d8c325a9ec98f7af0bc1adc0b30c86
Mathias Schmoigl <mathias.schmoigl@salzburgresearch.at>
2023-08-01 11:38:08 +0200

    added description for register defines

--- audio-codec-max9867/mtb_max9867.h
+++ audio-codec-max9867/mtb_max9867.h
@@ -47,76 +47,75 @@ extern "C"
 typedef enum
 {
 	// STATUS
-	MAX9867_STATUS = 0x00,		/* Status (Read Only) */
-	MAX9867_JACKSTATUS = 0x01, /* Jack Sense (Read Only) */
-	MAX9867_AUXHIGH = 0x02, /* AUX High (Read Only) */
-	MAX9867_AUXLOW = 0x03, /* AUX Low (Read Only) */
-	MAX9867_INTEN = 0x04, /* Interrupt Enable */
+	MAX9867_REG_STATUS = 0x00,		/* Status (Read Only) */
+	MAX9867_REG_JACKSTATUS = 0x01, /* Jack Sense (Read Only) */
+	MAX9867_REG_AUXHIGH = 0x02, /* AUX High (Read Only) */
+	MAX9867_REG_AUXLOW = 0x03, /* AUX Low (Read Only) */
+	MAX9867_REG_INTEN = 0x04, /* Interrupt Enable */
 
 	// CLOCK CONTROL
-	MAX9867_SYSCLK = 0x05, /* System Clock */
-	MAX9867_AUDIOCLKHIGH = 0x06, /* Stereo Audio Clock Control High */
-	MAX9867_AUDIOCLKLOW = 0x07, /* Stereo Audio Clock Control Low */
+	MAX9867_REG_SYSCLK = 0x05, /* System Clock */
+	MAX9867_REG_AUDIOCLKHIGH = 0x06, /* Stereo Audio Clock Control High */
+	MAX9867_REG_AUDIOCLKLOW = 0x07, /* Stereo Audio Clock Control Low */
 
 	// DIGITAL AUDIO INTERFACE
-	MAX9867_IFC1A = 0x08, /* Interface Mode */
-	MAX9867_IFC1B = 0x09, /* Interface Mode */
+	MAX9867_REG_IFC1A = 0x08, /* Interface Mode */
+	MAX9867_REG_IFC1B = 0x09, /* Interface Mode */
 
 	// DIGITAL FILTERING
-	MAX9867_CODECFLTR = 0x0a, /* Codec Filters */
+	MAX9867_REG_CODECFLTR = 0x0a, /* Codec Filters */
 
 	// LEVEL CONTROL
-	MAX9867_SIDETONE = 0x0b, /* Sidetone */
-	MAX9867_DACLEVEL = 0x0c, /* DAC Level */
-	MAX9867_ADCLEVEL = 0x0d, /* ADC Level */
-	MAX9867_LEFTLINELVL = 0x0e, /* Left-Line Input Level */
-	MAX9867_RIGHTLINELVL = 0x0f, /* Right-Line Input Level */
-	MAX9867_LEFTVOL = 0x10, /* Left Volume Control */
-	MAX9867_RIGHTVOL = 0x11, /* Right Volume Control */
-	MAX9867_LEFTMICGAIN = 0x12, /* Left Microphone Gain */
-	MAX9867_RIGHTMICGAIN = 0x13, /* Right Microphone Gain */
-	MAX9867_INPUTCONFIG = 0x14, /* CONFIGURATION / ADC Input */
-	MAX9867_MICCONFIG = 0x15, /* Microphone */
-	MAX9867_MODECONFIG = 0x16, /* Mode */
+	MAX9867_REG_SIDETONE = 0x0b, /* Sidetone */
+	MAX9867_REG_DACLEVEL = 0x0c, /* DAC Level */
+	MAX9867_REG_ADCLEVEL = 0x0d, /* ADC Level */
+	MAX9867_REG_LEFTLINELVL = 0x0e, /* Left-Line Input Level */
+	MAX9867_REG_RIGHTLINELVL = 0x0f, /* Right-Line Input Level */
+	MAX9867_REG_LEFTVOL = 0x10, /* Left Volume Control */
+	MAX9867_REG_RIGHTVOL = 0x11, /* Right Volume Control */
+	MAX9867_REG_LEFTMICGAIN = 0x12, /* Left Microphone Gain */
+	MAX9867_REG_RIGHTMICGAIN = 0x13, /* Right Microphone Gain */
+	MAX9867_REG_INPUTCONFIG = 0x14, /* CONFIGURATION / ADC Input */
+	MAX9867_REG_MICCONFIG = 0x15, /* Microphone */
+	MAX9867_REG_MODECONFIG = 0x16, /* Mode */
 
 	// POWER MANAGEMENT
-	MAX9867_PWRMAN = 0x17, /* System Shutdown */
-	MAX9867_REVISION = 0xff, /* Revision */
+	MAX9867_REG_PWRMAN = 0x17, /* System Shutdown */
+	MAX9867_REG_REVISION = 0xff, /* Revision */
 
 
 	// OTHERS (not part of regmap from datasheet)
-	MAX9867_PSCLK_10_20 = 0x1, /* System Clock's psclk value: 01 = Select if MCLK is between 10MHz and 20MHz. */
-	MAX9867_PSCLK_20_40 = 0x2, /* System Clock's psclk value: 10 = Select if MCLK is between 20MHz and 40MHz. */
-	MAX9867_PSCLK_40_60 = 0x3, /* System Clock's psclk value: 11 = Select if MCLK is between 40MHz and 60MHz. */
-	MAX9867_PSCLK_SHIFT= 0x4, /* psclk Shift */
-	MAX9867_PSCLK_WIDTH = 0x2, /* psclk Width */
+	MAX9867_VAL_PSCLK_10_20 = 0x1, /* System Clock's psclk value: 01 = Select if MCLK is between 10MHz and 20MHz. */
+	MAX9867_VAL_PSCLK_20_40 = 0x2, /* System Clock's psclk value: 10 = Select if MCLK is between 20MHz and 40MHz. */
+	MAX9867_VAL_PSCLK_40_60 = 0x3, /* System Clock's psclk value: 11 = Select if MCLK is between 40MHz and 60MHz. */
+	MAX9867_VAL_PSCLK_SHIFT= 0x4, /* psclk Shift */
+	MAX9867_VAL_PSCLK_WIDTH = 0x2, /* psclk Width */
+	MAX9867_VAL_RAPIDLOCK = 0x01, /* PLL’s rapid lock mode (Enabled: NI[0] = 1 / Disabled: NI[0] = 0) */
 
-	MAX9867_PSCLK_MASK = (0x03<<MAX9867_PSCLK_SHIFT), /*  */
-	MAX9867_FREQ_MASK = 0xF, /*  */
-	MAX9867_NI_HIGH_MASK = 0x7F, /*  */
-	MAX9867_NI_LOW_MASK	= 0xFE, /*  */
-	MAX9867_IFC1B_BCLK_MASK = 7, /*  */
+	MAX9867_MASK_PSCLK = (0x03<<MAX9867_PSCLK_SHIFT), /*  */
+	MAX9867_MASK_FREQ = 0xF, /*  */
+	MAX9867_MASK_NI_HIGH = 0x7F, /*  */
+	MAX9867_MASK_NI_LOW	= 0xFE, /*  */
+	MAX9867_MASK_IFC1B_BCLK = 7, /*  */
 
-	MAX9867_RAPID_LOCK = 0x01, /* PLL’s rapid lock mode (Enabled: NI[0] = 1 / Disabled: NI[0] = 0) */
+	MAX9867_BITSET_PLL	= (1<<7), /* Set PLL bit in register "Stereo Audio Clock Control High" */
+	MAX9867_BITSET_MASTER = (1<<7), /* Set MAS bit in register "Interface Mode" */
+	MAX9867_BITSET_CODECFLTR_MODE = (1<<7), /* Set MODE bit in register "Codec Filters" */
+	MAX9867_BITSET_PWRMAN_SHDN = (1<<7), /* Set SHDN bit in register "System Shutdown" */
+	MAX9867_BITSET_WCI_MODE = (1<<6), /* Set WCI bit in register "Interface Mode" */
+	MAX9867_BITSET_BCI_MODE = (1<<5), /* Set BCI bit in register "Interface Mode" */
+	MAX9867_BITSET_I2S_DLY = (1<<4), /* Set DLY bit in register "Interface Mode" */
+	MAX9867_BITSET_SDOUT_HIZ = (1<<3), /* Set HIZOFF bit in register "Interface Mode" */
+	MAX9867_BITSET_TDM_MODE = (1<<2), /* Set TDM bit in register "Interface Mode" */
 
-	MAX9867_PLL	= (1<<7), /* Set PLL bit in register "Stereo Audio Clock Control High" */
-	MAX9867_MASTER = (1<<7), /* Set MAS bit in register "Interface Mode" */
-	MAX9867_CODECFLTR_MODE = (1<<7), /* Set MODE bit in register "Codec Filters" */
-	MAX9867_PWRMAN_SHDN = (1<<7), /* Set SHDN bit in register "System Shutdown" */
-	MAX9867_WCI_MODE = (1<<6), /* Set WCI bit in register "Interface Mode" */
-	MAX9867_BCI_MODE = (1<<5), /* Set BCI bit in register "Interface Mode" */
-	MAX9867_I2S_DLY = (1<<4), /* Set DLY bit in register "Interface Mode" */
-	MAX9867_SDOUT_HIZ = (1<<3), /* Set HIZOFF bit in register "Interface Mode" */
-	MAX9867_TDM_MODE = (1<<2), /* Set TDM bit in register "Interface Mode" */
+	MAX9867_VAL_IFC1B_64X = 0x01, /* BSEL-Value: 001 = 64x LRCLK (192x internal clock divided by 3) */
+	MAX9867_VAL_IFC1B_48X = 0x02, /* BSEL-Value: 010 = 48x LRCLK (192x internal clock divided by 4) */
+	MAX9867_VAL_IFC1B_PCLK_2 = 0x04, /* BSEL-Value: 100 = PCLK/2 */
+	MAX9867_VAL_IFC1B_PCLK_4 = 0x05, /* BSEL-Value: 101 = PCLK/4 */
+	MAX9867_VAL_IFC1B_PCLK_8 = 0x06, /* BSEL-Value: 110 = PCLK/8 */
+	MAX9867_VAL_IFC1B_PCLK_16 = 0x07, /* BSEL-Value: 111 = PCLK/16 */
 
-	MAX9867_IFC1B_64X = 0x01, /* BSEL-Value: 001 = 64x LRCLK (192x internal clock divided by 3) */
-	MAX9867_IFC1B_48X = 0x02, /* BSEL-Value: 010 = 48x LRCLK (192x internal clock divided by 4) */
-	MAX9867_IFC1B_PCLK_2 = 0x04, /* BSEL-Value: 100 = PCLK/2 */
-	MAX9867_IFC1B_PCLK_4 = 0x05, /* BSEL-Value: 101 = PCLK/4 */
-	MAX9867_IFC1B_PCLK_8 = 0x06, /* BSEL-Value: 110 = PCLK/8 */
-	MAX9867_IFC1B_PCLK_16 = 0x07, /* BSEL-Value: 111 = PCLK/16 */
-
-	MAX9867_CACHEREGNUM = 10 /**/
+	MAX9867_VAL_CACHEREGNUM = 10 /**/
 
 } mtb_max9867_reg_t;
 
